dce416ef8c78385b71fb3a7bcb33a62fd0621dd9
11bus11 <02linfor01@gmail.com>
2026-04-06 17:43:53 +0200

    got .net running

--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -3,4 +3,9 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+//builder.Services.AddControllers();
+
+//string connectionString = builder.Configuration.GetConnectionString("Default") ?? throw new InvalidOperationException("Connection string 'Default' not found.");
+//builder.Services.AddDbContext<AppDbContext>(op=> op.UseSqlite(connectionString));
+
 app.Run();
