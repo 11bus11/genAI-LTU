62b68c3a1438034137b1acc4badc0d262b36f928
seberw-4-design <seberw-4@student.ltu.se>
2026-04-26 00:52:28 +0200

    Move database to Data folder

--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -1,15 +1,13 @@
 using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite("Data Source=app.db"));
+    options.UseSqlite("Data Source=Data/app.db"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-
-//builder.Services.AddControllers();
-
-//string connectionString = builder.Configuration.GetConnectionString("Default") ?? throw new InvalidOperationException("Connection string 'Default' not found.");
-//builder.Services.AddDbContext<AppDbContext>(op=> op.UseSqlite(connectionString));
 
 app.Run();
 
