a35677129b02185c67aa3bdaff1318944e60eca6
seberw-4-design <seberw-4@student.ltu.se>
2026-04-27 20:13:46 +0200

    Lägger till databasen i backend

--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -1,22 +1,22 @@
 using Google.GenAI;
 using Microsoft.Extensions.AI;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<GeminiService>();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=Data/app.db"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapGet("/ai", async(string prompt, GeminiService geminiService) =>
+app.MapGet("/ai", async (string prompt, GeminiService geminiService) =>
 {
     var result = await geminiService.GenerateTextAsync(prompt);
     return Results.Ok(result);
 });
-
-//builder.Services.AddControllers();
-
-//string connectionString = builder.Configuration.GetConnectionString("Default") ?? throw new InvalidOperationException("Connection string 'Default' not found.");
-//builder.Services.AddDbContext<AppDbContext>(op=> op.UseSqlite(connectionString));
 
 app.Run();
